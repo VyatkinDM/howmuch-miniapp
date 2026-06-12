07b778edbd0162afe30440d56a1839bd72317748
Dmytro Vyatkin <vyatkindmytro@MacBook-Air-Dmytro.local>
2026-06-13 00:51:16 +0300

    Show Telegram user data

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,17 @@
+import WebApp from '../services/telegram'
+
 export default function Dashboard() {
+  const user = WebApp.initDataUnsafe?.user
+
   return (
     <div style={{ padding: 20 }}>
       <h1>💰 HowMuch</h1>
 
-      <p>Mini App працює</p>
+      <h2>Mini App працює</h2>
+
+      <pre>
+        {JSON.stringify(user, null, 2)}
+      </pre>
     </div>
   )
 }
